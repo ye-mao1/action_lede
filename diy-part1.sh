97eac7a51af19f0edb63c77f73b01e993321cfa5
ye-mao1 <67098619+ye-mao1@users.noreply.github.com>
2021-08-11 16:45:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,3 +19,4 @@
 sed -i '$a src-git lienol http://github.com/Lienol/openwrt-package' feeds.conf.default
 git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/MentoHUST-OpenWrt-ipk
 git clone https://github.com/BoringCat/luci-app-mentohust.git package/luci-app-mentohust
+https://github.com/pymumu/smartdns.git package/smartdns
